f6e87ec8bfb6c59df959b8ccbc8e406a65c5ab46
Swishyfishie <pclandreiuk@gmail.com>
2020-10-22 05:08:41 +0100

    add user component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react'
 
 class UserItem extends Component {
 
-    constructor(){
-        super()
-        this.state = {
-            id: 'id',
-            login: 'mojombo',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-            html_url: 'https://github.com/mojombo'
-        }
-    }
+    // constructor(){
+    //     super()
+    //     this.state = {
+    //         id: 'id',
+    //         login: 'mojombo',
+    //         avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    //         html_url: 'https://github.com/mojombo'
+    //     }
+    // }
 
     render() {
-        const { login, avatar_url, html_url} = this.state
+        const { login, avatar_url, html_url} = this.props.user
         return (
             <div className="card text-center">
                 <img src={avatar_url} alt="user avatar" className="round-img" style={{width: '60px'}}/>
